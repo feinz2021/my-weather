f43964bfa61f84676ffa9ce22da3cb82dbcacdc5
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-21 17:23:13 +0800

    update

--- src/components/WeatherPage.vue
+++ src/components/WeatherPage.vue
@@ -1,6 +1,54 @@
 <template>
   <div class="container">
-    <!-- <pre>{{ temp }}</pre> -->
+    <button class="btn" @click="getGeneralForecast()">General Forecast</button>
+    <button class="btn" @click="getDataTypes()">Get Data Types</button>
+    <br />
+
+    <table>
+      <thead>
+        <tr>
+          <th>locationrootname</th>
+          <th>locationname</th>
+          <th>date</th>
+          <th>datatype</th>
+          <th>value</th>
+          <th>latitude</th>
+          <th>longitude</th>
+          <th>attributes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="id in generalForecast" v-bind:key="id">
+          <td>{{ id.locationrootname }}</td>
+          <td>{{ id.locationname }}</td>
+          <td>{{ id.date }}</td>
+          <td>{{ id.datatype }}</td>
+          <td>{{ id.value }}</td>
+          <td>{{ id.latitude }}</td>
+          <td>{{ id.longitude }}</td>
+          <td>{{ id.attributes.code }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>datasetid</th>
+          <th>datacategoryid</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="id in dataTypes" v-bind:key="id">
+          <td>{{ id.id }}</td>
+          <td>{{ id.name }}</td>
+          <td>{{ id.datasetid }}</td>
+          <td>{{ id.datacategoryid }}</td>
+        </tr>
+      </tbody>
+    </table>
 
     <table>
       <thead>
@@ -9,9 +57,8 @@
           <th>Name</th>
         </tr>
       </thead>
-
       <tbody>
-        <tr v-for="locationID in temp" v-bind:key="locationID">
+        <tr v-for="locationID in location" v-bind:key="locationID">
           <td>{{ locationID.id }}</td>
           <td>{{ locationID.name }}</td>
         </tr>
@@ -25,27 +72,59 @@ import axios from "axios";
 export default {
   data() {
     return {
-      temp: [],
+      location: [],
+      generalForecast: [],
+      headerToken: {},
+      dataTypes: [],
     };
+  },
+  methods: {
+    getGeneralForecast() {
+      axios
+        .get(
+          "https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid=LOCATION:246&start_date=2022-06-21&end_date=2022-06-21",
+          {
+            headers: this.headerToken,
+          }
+        )
+        .then((res) => {
+          for (let i = 0; i < 4; +i++) {
+            let resultsLength = res.data.results.length;
+            console.log(resultsLength);
+            for (let j = 0; j < resultsLength; j++) {
+              this.generalForecast.push(res.data.results[j]);
+            }
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    getDataTypes() {
+      axios
+        .get("https://api.met.gov.my/v2.1/datatypes", {
+          headers: this.headerToken,
+        })
+        .then((res) => {
+          for (let i = 0; i < 4; +i++) {
+            let resultsLength = res.data.results.length;
+            console.log(resultsLength);
+            for (let j = 0; j < resultsLength; j++) {
+              this.dataTypes.push(res.data.results[j]);
+            }
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
   mounted() {
     window.M.AutoInit();
-    // axios
-    //   .get(
-    //     // "https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid=LOCATION:237&start_date=2022-06-21&end_date=2022-06-21",
-    //     "https://api.met.gov.my/v2.1/locations?locationcategoryid=DISTRICT&offset=163",
-    //     {
-    //       headers: {
-    //         Authorization: `METToken 79a6cd59e081e56a1aea2335888829e118dfa29b`,
-    //       },
-    //     }
-    //   )
-    //   .then((res) => {
-    //     this.temp = res;
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
+
+    this.headerToken = {
+      Authorization: `METToken 79a6cd59e081e56a1aea2335888829e118dfa29b`,
+    };
 
     let URL1 =
       "https://api.met.gov.my/v2.1/locations?locationcategoryid=DISTRICT";
@@ -56,9 +135,7 @@ export default {
     let URL4 =
       "https://api.met.gov.my/v2.1/locations?locationcategoryid=DISTRICT&offset=150";
     let header = {
-      headers: {
-        Authorization: `METToken 79a6cd59e081e56a1aea2335888829e118dfa29b`,
-      },
+      headers: this.headerToken,
     };
 
     const promise1 = axios.get(URL1, header);
@@ -73,7 +150,7 @@ export default {
           let resultsLength = res[i].data.results.length;
           console.log(resultsLength);
           for (let j = 0; j < resultsLength; j++) {
-            this.temp.push(res[i].data.results[j]);
+            this.location.push(res[i].data.results[j]);
           }
         }
       })
